26507e5cba8b7f3e708e19d7b42ecb400a1d0089
Lych <Aqu4men@gmail.com>
2023-11-28 19:25:37 +0100

    menu

--- SKAL/Assets/Scripts/Menu/MainMenu.cs
+++ SKAL/Assets/Scripts/Menu/MainMenu.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void LaunchGame()
     {
+        SceneManager.LoadScene("GameScene", LoadSceneMode.Single);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 
 }
